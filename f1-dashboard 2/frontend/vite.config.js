82323ffea1115cf7e8bb82ec0d3fef5b2e054b77
MarcosGibert <margiro16180@gmail.com>
2026-07-05 10:50:45 +0200

    Delete f1-dashboard 2 directory

--- f1-dashboard 2/frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base './' + HashRouter = works on GitHub Pages project sites without config
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
